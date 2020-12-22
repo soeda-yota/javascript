9643046aac426a6519fade0d7fdc514ec4edb4bf
soeda-yota <74278998+soeda-yota@users.noreply.github.com>
2020-12-22 10:25:13 +0900

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,12 @@
+var x = 34;
+var y = -0.25 * x + 14.1;
+document.writeln(y);
+
+var radius = 12;
+var area = radius * radius * 3.14;
+document.writeln(area);
+
+let w = 10;
+let h = 17;
+let triangleArea = w * h / 2;
+document.writeln(triangleArea);
