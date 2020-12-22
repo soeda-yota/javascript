dbbbfeb695fa9d917bc332cb49b08ae60583a5f5
soeda-yota <74278998+soeda-yota@users.noreply.github.com>
2020-12-22 09:33:22 +0900

    Update object.js

--- object.js
+++ object.js
@@ -1,2 +1,19 @@
-let displayArea = document.getElementById('display-area');
-displayArea.innerText = '2020最高でした';
+let startTime;
+  let endTime;
+  const displayArea = document.getElementById('display-area');
+  function start() {
+    startTime = Date.now();
+    document.body.onkeypress = stop;
+  }
+  function stop() {
+    endTime = Date.now();
+    let result = (endTime - startTime) / 1000;
+    if (9.5 <= result && result <= 10.5) {
+      displayArea.innerText　= `${result}秒でした。素晴らしい`;
+    } else {
+      displayArea.innerText = `${result}秒でした。残念`;
+    }
+  }
+  if (confirm(`10秒だと思ったら何かキーを押してください`)){
+    start();
+  }
